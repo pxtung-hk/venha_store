40c5fe5f8d104be00e7af18d2fdac495e3a6769e
pxtung-hk <honkai02zerotwo@gmail.com>
2024-01-05 11:29:28 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import Footerbottom from "./components/Footerbottom";
+import Header from "./components/Header";
+import Product from "./components/Product";
+import Slider from "./components/Slider";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () =>{
+
+  return(
+    <>
+    <Header/>
+    <Body/>
+    <Slider/>
+    <Product/>
+    <Footer/>
+    <Footerbottom/>
+    </>
+  )
 }
-
 export default App;
